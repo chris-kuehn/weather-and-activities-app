322d1b0c44cd53eaa3963c8041e2270429de1417
Christian Kühn <christian.kuhn.istanbul@gmail.com>
2023-07-25 15:55:08 +0200

    create form and list

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 export default function Form({ onAddActivity }) {
+  // const [name, setName] = useState("");
+  // const [isForGoodWeather, setIsForGoodWeather] = useState(true);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // console.log(event.target.name.value);
+    // console.log(event.target.checkbox.checked);
+
+    const newActivity = {
+      activityName: event.target.name.value,
+      isForGoodWeather: event.target.checkbox.checked,
+    };
+    onAddActivity(newActivity);
+    // console.log(newActivity);
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add new activity:</h2>
-      <label for="name">Name:</label>
-      <input type="text" name="name"></input>
-      <label for="checkbox">Good werather activity:</label>
-      <input type="checkbox" name="checkbox"></input>
-      <button>Submit</button>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" />
+      <label htmlFor="checkbox">Good werather activity:</label>
+      <input type="checkbox" name="checkbox" id="checkbox" />
+      <button type="submit">Submit</button>
     </form>
   );
 }
